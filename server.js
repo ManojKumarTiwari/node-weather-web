b2b5f41b4dc28b98bfe47b03b6a66dd66133f5f5
Manoj Kumar Tiwari <manoj.tiwari@e4eie.com>
2018-04-21 00:08:09 +0530

    add post flow

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
 
@@ -11,4 +14,9 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
 })
